379538b8980cbb7965a6b19a94b0aa2752d6f392
King Lemming <kinglemming@gmail.com>
2021-08-03 19:19:48 -0700

    Switching to dev.
    Signed-off-by: King Lemming <kinglemming@gmail.com>

--- src/main/java/cofh/thermal/dynamics/ThermalDynamics.java
+++ src/main/java/cofh/thermal/dynamics/ThermalDynamics.java
@@ -15,8 +15,8 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 import static cofh.lib.util.constants.Constants.ID_THERMAL_DYNAMICS;
 import static cofh.thermal.core.ThermalCore.BLOCKS;
-import static cofh.thermal.core.init.TCoreIDs.ID_DEVICE_COLLECTOR;
-import static cofh.thermal.core.init.TCoreIDs.ID_DEVICE_NULLIFIER;
+import static cofh.thermal.lib.common.ThermalIDs.ID_DEVICE_COLLECTOR;
+import static cofh.thermal.lib.common.ThermalIDs.ID_DEVICE_NULLIFIER;
 import static cofh.thermal.dynamics.init.TDynIDs.ID_ENDER_TUNNEL;
 import static cofh.thermal.dynamics.init.TDynReferences.LOGISTICS_ITEM_BUFFER_CONTAINER;
 import static cofh.thermal.lib.common.ThermalFlags.FLAG_XP_STORAGE_AUGMENT;
