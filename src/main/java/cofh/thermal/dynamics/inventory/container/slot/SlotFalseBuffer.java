8ca60c2e87afce7fbe05e2718e37209d15c12142
King Lemming <kinglemming@gmail.com>
2023-05-02 22:00:35 -0700

    Update SlotFalseBuffer.java

--- src/main/java/cofh/thermal/dynamics/inventory/container/slot/SlotFalseBuffer.java
+++ src/main/java/cofh/thermal/dynamics/inventory/container/slot/SlotFalseBuffer.java
@@ -4,6 +4,8 @@ import cofh.lib.inventory.container.slot.SlotFalseCopy;
 import net.minecraft.world.Container;
 import net.minecraft.world.item.ItemStack;
 
+import static cofh.core.util.helpers.ItemHelper.cloneStack;
+
 /**
  * Slot which copies an ItemStack when clicked on, does not decrement the ItemStack on the cursor.
  */
@@ -22,6 +24,24 @@ public class SlotFalseBuffer extends SlotFalseCopy {
         }
         container.setItem(this.slot, stack);
         setChanged();
+    }
+
+    public void incrementStack(int count) {
+
+        ItemStack myStack = getItem();
+        if (!myStack.isEmpty() && myStack.getCount() < myStack.getMaxStackSize()) {
+            container.setItem(this.slot, cloneStack(myStack, Math.min(myStack.getCount() + count, myStack.getMaxStackSize())));
+            setChanged();
+        }
+    }
+
+    public void decrementStack(int count) {
+
+        ItemStack myStack = getItem();
+        if (!myStack.isEmpty() && myStack.getCount() > 1) {
+            container.setItem(this.slot, cloneStack(myStack, Math.max(myStack.getCount() - count, 1)));
+            setChanged();
+        }
     }
 
 }
